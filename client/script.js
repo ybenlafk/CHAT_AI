d0a58b2106b348d6396d619543ba7f50fc781453
Yahya Benlafkih <ybenlafk@e1r8p8.1337.ma>
2023-02-12 14:59:27 +0100

    :)

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('https://ai-chat-2yez.onrender.com', {
+    const response = await fetch('https://chat-ai-z4b7.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
